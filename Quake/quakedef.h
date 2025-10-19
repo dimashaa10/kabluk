f2c886127d24b5ab260fa1e719fd3d4b43d55e85
Your a <a@example.com>
2025-10-19 23:57:00 +0700

    0.1.7 casino update 3

--- Quake/quakedef.h
+++ Quake/quakedef.h
@@ -44,7 +44,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #define QSS_VER	"19-10-25" // woods
 
-// woods add qss-m versions info
+// kabluk version info
 
 #define QSSM_VER_MAJOR		0
 #define QSSM_VER_MINOR		1
